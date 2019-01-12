08b7fe10e250be44a201f9d2329c9ada0d4980a1
IEkAdN <9sh9shkn10chh@gmail.com>
2019-01-12 16:01:27 +0900

    Add files via upload

--- benchmark/get_snp_phenix/fuga.h
+++ benchmark/get_snp_phenix/fuga.h
@@ -5,15 +5,18 @@
 
 class Fuga {
  public:
-  Fuga(string FNom) : kFNom_(FNom) {}
+  Fuga(string PhenixFaNom, string RefFaNom) : kPhenixFaNom_(PhenixFaNom), kRefFaNom_(RefFaNom) {}
   ~Fuga() {}
   void Main();
 
  private:
-  const string kFNom_;
-  void Read();
+  const string kPhenixFaNom_;
+  const string kRefFaNom_;
+  void ReadPhenixFa();
+  void ReadRefFa();
   void Fa2Snp();
   map<string, string> Fa_;
+  string RefChrId_;
 };
 
 #endif  // FUGA_H_
--- benchmark/get_snp_phenix/main.cc
+++ benchmark/get_snp_phenix/main.cc
@@ -1,11 +1,11 @@
 #include "fuga.h"
 
 int main(int argc, const char* argv[]) {
-  if (argc != 2) {
-    cerr << "usage: a []\n";
+  if (argc != 3) {
+    cerr << "usage: a [phynix.fasta] [ref.fasta]\n";
     return 1;
   } else {
-    Fuga fuga(argv[1]);
+    Fuga fuga(argv[1], argv[2]);
     fuga.Main();
   }
   return 0;
